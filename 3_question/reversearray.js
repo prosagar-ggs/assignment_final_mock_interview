3417730bed29e9fd954443883f17b88f3e67b003
prosagar-ggs <gonegangasagar@gmail.com>
2022-09-07 20:24:54 +0530

    final_assignment

--- 3_question/reversearray.js
+++ 3_question/reversearray.js
@@ -1,5 +1,8 @@
-let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,],n=5,reqarr=[]
+let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,],n=9,reqarr=[]
 for(let i=arr.length-1; i>=n; i--){
+    reqarr.push(arr[i])
+}
+for(let i=0; i<n; i++){
     reqarr.push(arr[i])
 }
 console.log(reqarr)
